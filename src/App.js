88a5b3fb5f9bb76ea8f80f16a319cddec1777928
elavarasan03e <elu03.e@gmail.com>
2024-05-04 18:28:25 +0530

    counter class react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import React, { Component } from 'react';
 import './App.css';
-import {useState} from'react'
 
-function App() {
-  const [count,setCount]=useState(0);
-  const handleIncerment=()=>{
-    setCount(count+1);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
   }
-  const handleDecrement=()=>{
-    setCount(count-1);
-  }
-  return (
-    <div className="App">
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={handleIncerment}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
 
-    </div>
-  );
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      count: prevState.count - 1
+    }));
+  }
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Counter App</h1>
+        <p>Count: {count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
 }
 
 export default App;
